f921954bcdcabbcc1b3622a417732a0524084433
panmeibing <panmeibing@bitforex.email>
2023-07-11 18:08:24 +0800

    switch to the wxlist api

--- baiduwp_python/baiduwp_python/utils/selenium_utils.py
+++ baiduwp_python/baiduwp_python/utils/selenium_utils.py
@@ -4,13 +4,13 @@ from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver.chrome.service import Service
 
 from baiduwp_python.settings.settings import logger
-from .singleton_utils import singleton
+# from .singleton_utils import singleton
 from baiduwp_python.settings.config import selenium_executable_path
 
 
-@singleton
-class SeleniumDriver:
-    def __init__(self, executable_path):
+def get_chrome_driver(executable_path):
+    driver = None
+    try:
         options = ChromeOptions()
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
@@ -19,24 +19,33 @@ class SeleniumDriver:
         options.add_argument("blink-settings=imagesEnabled=false")
         options.add_argument("--disable-extensions")
         options.add_argument("--ignore-certificate-errors")
-        options.add_argument("--headless")
+        # options.add_argument("--headless")
         s = Service(executable_path=executable_path)
-        self.driver = Chrome(service=s, options=options)
-
-    def get_driver(self):
-        return self.driver
-
-    def __del__(self):
-        if self.driver:
-            self.driver.quit()
+        driver = Chrome(service=s, options=options)
+        source_str = """Object.defineProperty(navigator, 'webdriver', {get: () => undefined })"""
+        driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {"source": source_str})
+    except Exception as e:
+        logger.error(e)
+    return driver
 
 
-def get_html_by_selenium(url):
+def get_html_by_selenium(url, bduss):
     try:
-        driver = SeleniumDriver(selenium_executable_path).get_driver()
+        driver = get_chrome_driver(selenium_executable_path)
+        driver.get("https://www.baidu.com")
+        driver.add_cookie({"name": "BDUSS", "value": bduss})
+        driver.refresh()
+        time.sleep(1)
         driver.get(url)
-        time.sleep(3)
+        print("get_cookies: ", driver.get_cookies())
+        # time.sleep(5)
         return driver.execute_script("return document.documentElement.outerHTML")
     except Exception as e:
         logger.error(f"get_html_by_selenium() meet error: {e}")
         return ""
+
+
+if __name__ == '__main__':
+    bd_uss = "ZYOTJVTU1oM01PendPT0NYOHkxemZRTjJmVnhlZG1mMlhFaE5FdUFqVW5kSkprSVFBQUFBJCQAAAAAAAAAAAEAAACgtwcpsfnA5LXEz6PN-zEyMwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACfnamQn52pkWT"
+    html_str = get_html_by_selenium("https://pan.baidu.com/s/1TlDb_fQ6NlaTMUEZbib4Bw?pwd=bk8d", bd_uss)
+    print("html_str:", html_str)
